e328c15e91a161fe3f9b6e7cb9d998fb6663d72a
Ajay <jaykumar002kori@gmail.com>
2024-04-20 14:15:48 +0530

    commit

--- src/components/data_intake.py
+++ src/components/data_intake.py
@@ -1,11 +1,1 @@
-from common.utility import modelpreprocessing
-
-model=modelpreprocessing    ()
-
-text='try to save text file'
-
-file='text.pkl'
-
-
-res=model.save_obj(file, text)
-print(res)
+from common.utility import save_obj
