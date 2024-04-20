64a5ee30a0f90a1b43b95407f981fad696661173
Ajay <jaykumar002kori@gmail.com>
2024-04-20 16:45:43 +0530

    commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='RedWineQuality',
-    version='0.0.2',
+    version='0.0.3',
     author="Ajay",
     author_email="jaykumar002kori@gmail.com",
     packages=find_packages(),
